b7c9571bf5f3a0e7b6151980c7b53acdc02ff56a
B. Endres <endres@systopia.de>
2019-03-06 07:33:26 +0100

    added data structure

---
+++ CRM/Contactlinks/Upgrader.php
@@ -0,0 +1,36 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA ContactLinks Extension                        |
+| Copyright (C) 2019 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
+| http://www.systopia.de/                                |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+use CRM_Contactlinks_ExtensionUtil as E;
+
+/**
+ * Collection of upgrade steps.
+ */
+class CRM_Contactlinks_Upgrader extends CRM_Contactlinks_Upgrader_Base {
+
+  /**
+   * Installer
+   */
+  public function install() {
+    // run the custom group sync
+    require_once 'CRM/Contactlinks/CustomData.php';
+    $customData = new CRM_Contactlinks_CustomData('de.systopia.contactlinks');
+    $customData->syncOptionGroup(__DIR__ . '/../../resources/contact_link_category_option_group.json');
+    $customData->syncCustomGroup(__DIR__ . '/../../resources/contactlinks_custom_group.json');
+
+    return TRUE;
+  }
+}
